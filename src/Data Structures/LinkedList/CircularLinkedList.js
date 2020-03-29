33751515d9df48f0968a9d83abcea3c11178f547
Vikash Gupta <vikashgupta@urbanclap.com>
2020-03-29 17:00:00 +0530

    changes

--- src/Data Structures/LinkedList/CircularLinkedList.js
+++ src/Data Structures/LinkedList/CircularLinkedList.js
@@ -1,14 +1,78 @@
 class LinkedListNode {
-  constructor(data, previous, next) {
+  constructor(data, next) {
     this.data = data;
-    this.previous = previous;
     this.next = next;
   }
 }
-
 class CircularLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+  }
+  prepend(data) {
+    const node = new LinkedListNode(data);
+    if (this.head) {
+      node.next = this.head;
+      this.tail.next = node;
+      this.head = node;
+    } else {
+      node.next = node;
+      this.head = node;
+      this.tail = node;
+    }
+    return this;
+  }
+  append(data) {
+    const node = new LinkedListNode(data);
+    if (this.tail) {
+      node.next = this.head;
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      return this.prepend(data);
+    }
+    return this;
+  }
+  deleteLast() {
+    if (this.head) {
+      let prev = this.head;
+      let curr = this.head;
+      while(curr.next !== this.head) {
+        prev = curr;
+        curr = curr.next;
+      }
+      prev.next = curr.next;
+      this.tail = prev;
+    }
+    return this;
+  }
+  deleteFirst() {
+    if (this.head) {
+      this.tail.next = this.head.next;
+      this.head = this.head.next;
+    }
+    return this;
+  }
+  count() {
+    let current = this.head;
+    let count = 0;
+    do {
+      ++count;
+      current = current.next;
+    } while(current !== this.head);
+    return count;
+  }
+  print() {
+    let currentNode = this.head;
+    while(currentNode) {
+      console.log(currentNode.data);
+      if (
+        currentNode.next === currentNode ||
+        currentNode.next === this.head
+      ) {
+        break;
+      }
+      currentNode = currentNode.next;
+    }
   }
 }
--- src/Data Structures/LinkedList/README.md
+++ src/Data Structures/LinkedList/README.md
@@ -1,3 +1,11 @@
 # Linked List
 
 Linked list is used to store collection of data very much like arrays.
+
+Types:
+- [x] Singly Linked List
+- [x] Doubly Linked List
+- [x] Circular Linked List
+- [ ] Unrolled Linked List
+- [ ] Skip List
+
